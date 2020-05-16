82632edb7b06096d39f088fb09ce67ad413143ab
Duong Kieu <duong.kieu@defide-ix.com>
2020-05-16 23:27:43 +0700

    [f] Finished auth service

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -7,10 +7,15 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  console.log("found error");
+  console.log(err instanceof CustomError);
+
   if (err instanceof CustomError) {
     console.log(err);
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
+
+  console.log(err);
 
   res.status(400).send({
     errors: [{ message: "Something went wrong" }],
