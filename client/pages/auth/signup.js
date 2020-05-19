e6c2d02ec4179d3dee2fe688734ca67189b67516
Duong Kieu <duong.kieu@defide-ix.com>
2020-05-20 05:56:14 +0700

    [r] Change to single quote

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import axios from "axios";
+import { useState } from 'react';
+import axios from 'axios';
 
 const signup = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [errors, setErrors] = useState([]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post("/api/users/signup", {
+      const response = await axios.post('/api/users/signup', {
         email,
         password,
       });
@@ -23,27 +23,27 @@ const signup = () => {
   return (
     <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
-      <div className="form-goup">
+      <div className='form-goup'>
         <label>Email address</label>
         <input
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="form-control"
+          className='form-control'
         />
       </div>
-      <div className="form-group">
+      <div className='form-group'>
         <label>Password</label>
         <input
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          className="form-control"
+          type='password'
+          className='form-control'
         />
       </div>
       {errors.length > 0 && (
-        <div className="alert alert-danger">
+        <div className='alert alert-danger'>
           <h4>Ooops...</h4>
-          <ul className="my-0">
+          <ul className='my-0'>
             {errors.map((err) => (
               <li key={err.message}>{err.message}</li>
             ))}
@@ -51,7 +51,7 @@ const signup = () => {
         </div>
       )}
 
-      <button className="btn btn-primary">Sign Up</button>
+      <button className='btn btn-primary'>Sign Up</button>
     </form>
   );
 };
